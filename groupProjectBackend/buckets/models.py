5cd634e1ec6b893d98912078b95b833f851649ec
Michelle Gleed <gleed.michelle@gmail.com>
2020-10-24 10:58:08 +0800

    Added models

--- groupProjectBackend/buckets/models.py
+++ groupProjectBackend/buckets/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Bucket(models.Model):
+    pass
+
+class Transaction(models.Model):
+    pass
+
+class Icon(models.Model):
+    pass
