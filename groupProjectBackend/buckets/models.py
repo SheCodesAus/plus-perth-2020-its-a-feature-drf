c66e6e7776513cd86617c34312caa13c98f82c03
alisonvpereira <alisonvpereira@gmail.com>
2020-10-24 11:53:02 +0800

    added models to bucket apps

--- groupProjectBackend/buckets/models.py
+++ groupProjectBackend/buckets/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Bucket(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+    is_active = models.BooleanField()
+    min_amt = models.FloatField()
+    percentage = models.IntegerField()
+    parent_bucket = models.IntegerField()
 
 class Transaction(models.Model):
-    pass
+    income = models.FloatField()
+    date_created =  models.DateTimeField(auto_now_add=True)
 
 class Icon(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    image = models.URLField()
