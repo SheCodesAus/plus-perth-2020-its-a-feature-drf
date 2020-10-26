7f8fbd2534ebeaab41084b6e41117b20908c21a1
Michelle Gleed <gleed.michelle@gmail.com>
2020-10-26 14:37:40 +0800

    Added more meaningful Integrity errors for SignUp

--- groupProjectBackend/users/views.py
+++ groupProjectBackend/users/views.py
@@ -5,6 +5,9 @@ from rest_framework import status
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
+from django.db import IntegrityError
+from rest_framework.exceptions import ParseError
+
 class CustomUserList(APIView):
     def get(self, request):
         users = CustomUser.objects.all()
@@ -13,10 +16,21 @@ class CustomUserList(APIView):
 
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+
+            email = serializer.validated_data.get('email')
+            try:
+                # checking for any pre-existing users with this email. If found, continues to next line and raises Error. If not found, raises CustomUser.DoesNotExist exception, in which case it is ok to save this new user! We put the serializer.save() method inside the exception.
+                nonUniqueUser = CustomUser.objects.get(email=email)
+                raise ParseError(detail="This email is already signed up")
+            except CustomUser.DoesNotExist:
+                try:
+                    serializer.save()
+                    return Response(serializer.data)
+                except IntegrityError:
+                    # checking this instance of CustomUser doesn't already exist (ie. same username). If username not found in system, save. Else, raise Error
+                    raise ParseError(detail="This username already exists")
 
 
 class CustomUserDetail(APIView):
